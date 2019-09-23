66df8b3897f3f07c7830b3d3f421d899f6e578e1
Andrea Z <developer02@sinmaf.com.co>
2019-09-23 01:10:40 -0400

    Problem 5 (Project + Notebook with questions)

--- Homework 2/Problem5.py
+++ Homework 2/Problem5.py
@@ -3,9 +3,60 @@ import random
 import numpy as np
 
 
+def g_delta(w, xi, yi, delta):
+    xi = np.array([xi])
+    yi = np.array([yi])
+    prediction = np.dot(xi, w)
+    if (prediction - yi) <= -delta:
+        return np.square(prediction - yi + delta)
+    elif abs(yi - prediction) < delta:
+        return 0
+    else:
+        return np.square(prediction - yi - delta)
+
+
+def g_delta_derivative(w, xi, yi, delta):
+    xi = np.array([xi])
+    yi = np.array([yi])
+    prediction = np.dot(xi, w)
+    if (prediction - yi) <= -delta:
+        return 2 * np.dot(xi.T, prediction - yi + delta)
+    elif abs(prediction - yi) < delta:
+        return np.zeros(w.shape)
+    else:
+        return 2 * np.dot(xi.T, prediction - yi - delta)
+
+
+def f_w(w, x, y, delta, lam):
+    n = np.size(x, 0)
+    g_delta_x = np.array([g_delta(w, xi, yi, delta) for xi, yi in zip(x, y)])
+    return (1/n) * np.sum(g_delta_x) + np.sum(np.square(w))
+
+
+def f_w_derivative(w, x, y, delta, lam):
+    n = np.size(x, 0)
+    g_delta_derivative_x = np.array([g_delta_derivative(w, xi, yi, delta) for xi, yi in zip(x, y)])
+    return (1 / n) * np.sum(g_delta_derivative_x, 0) + 2 * lam * w
+
+
 def bgd_l2(data, y, w, eta, delta, lam, num_iter):
-    return new_w, history_fw
+    history_fw = np.zeros(num_iter)
+    n = np.size(data, 0)
+    x = np.c_[np.ones(n), data]
+    for it in range(num_iter):
+        prediction = f_w_derivative(w, x, y, delta, lam)
+        w = w - eta * prediction
+        history_fw[it] = f_w(w, x, y, delta, lam)
+    return w, history_fw
 
 
 def sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
-    return new_w, history_fw
+    history_fw = np.zeros(num_iter)
+    n = np.size(data, 0)
+    x = np.c_[np.ones(n), data]
+    for it in range(1, num_iter):
+        ix = random.choice(np.arange(np.size(x, 0))) if i == -1 else i
+        prediction = f_w_derivative(w, np.array([x[ix]]), np.array([y[ix]]), delta, lam)
+        w = w - eta/math.sqrt(it) * prediction
+        history_fw[it] = f_w(w, x, y, delta, lam)
+    return w, history_fw
--- Homework 2/Problem5Plot.py
+++ Homework 2/Problem5Plot.py
@@ -1,7 +1,69 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import Problem5 as gd
 
 if __name__ == '__main__':
     # Put the code for the plots here, you can use different functions for each
     # part
+    data = np.load("data.npy")
+    x = data[:, 0].reshape(-1, 1)
+    y = data[:, 1].reshape(-1, 1)
+    std_scale = preprocessing.StandardScaler().fit(x)
+    x = std_scale.transform(x)
+    w = np.array([[0], [1]])
+
+    w1, fw1_history = gd.bgd_l2(x, y, w, eta=0.05, delta=0.1, lam=0.001, num_iter=50)
+    w2, fw2_history = gd.bgd_l2(x, y, w, eta=0.1, delta=0.01, lam=0.001, num_iter=50)
+    w3, fw3_history = gd.bgd_l2(x, y, w, eta=0.1, delta=0, lam=0.001, num_iter=100)
+    w4, fw4_history = gd.bgd_l2(x, y, w, eta=0.1, delta=0, lam=0, num_iter=100)
+
+    fig, ax = plt.subplots(4, 2, figsize=(15,12))
+    ax[0, 0].plot(fw1_history, 'red')
+    ax[0, 0].set_title('GD: eta=0.05, delta=0.1, lam=0.001, num_iter=50')
+    ax[0, 0].set_xlabel('Iterations')
+    ax[0, 0].set_ylabel('fw')
+
+    ax[0, 1].plot(fw2_history, 'red')
+    ax[0, 1].set_title('GD: eta=0.1, delta=0.01, lam=0.001, num_iter=50')
+    ax[0, 1].set_xlabel('Iterations')
+    ax[0, 1].set_ylabel('fw')
+
+    ax[1, 0].plot(fw3_history, 'red')
+    ax[1, 0].set_title('GD: eta=0.1, delta=0, lam=0.001, num_iter=100')
+    ax[1, 0].set_xlabel('Iterations')
+    ax[1, 0].set_ylabel('fw')
+
+    ax[1, 1].plot(fw4_history, 'red')
+    ax[1, 1].set_title('GD: eta=0.1, delta=0, lam=0, num_iter=100')
+    ax[1, 1].set_xlabel('Iterations')
+    ax[1, 1].set_ylabel('fw')
+
+    w5, fw5_history = gd.sgd_l2(x, y, w, eta=1, delta=0.1, lam=0.5, num_iter=800)
+    w6, fw6_history = gd.sgd_l2(x, y, w, eta=1, delta=0.01, lam=0.1, num_iter=800)
+    w7, fw7_history = gd.sgd_l2(x, y, w, eta=1, delta=0, lam=0, num_iter=40)
+    w8, fw8_history = gd.sgd_l2(x, y, w, eta=1, delta=0, lam=0, num_iter=800)
+
+    ax[2, 0].plot(fw5_history, 'red')
+    ax[2, 0].set_title('SGD: eta=1, delta=0.1, lam=0.5, num_iter=800')
+    ax[2, 0].set_xlabel('Iterations')
+    ax[2, 0].set_ylabel('fw')
+
+    ax[2, 1].plot(fw6_history, 'red')
+    ax[2, 1].set_title('SGD: eta=1, delta=0.01, lam=0.1, num_iter=800')
+    ax[2, 1].set_xlabel('Iterations')
+    ax[2, 1].set_ylabel('fw')
+
+    ax[3, 0].plot(fw7_history, 'red')
+    ax[3, 0].set_title('SGD: eta=1, delta=0, lam=0, num_iter=40')
+    ax[3, 0].set_xlabel('Iterations')
+    ax[3, 0].set_ylabel('fw')
+
+    ax[3, 1].plot(fw8_history, 'red')
+    ax[3, 1].set_title('SGD: eta=1, delta=0, lam=0, num_iter=800')
+    ax[3, 1].set_xlabel('Iterations')
+    ax[3, 1].set_ylabel('fw')
+
+    fig.subplots_adjust(hspace=1.5)
+    plt.show()
